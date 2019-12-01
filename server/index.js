b1a65e7b7e8f252cde117d49088fe8ede895a88d
Yohan <yohan@yohanpro.com>
2019-12-01 14:08:01 +0900

    server body parser 이용, api 보내기 테스트

--- server/index.js
+++ server/index.js
@@ -10,12 +10,45 @@ const express = require('express');
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({
     dev
-})
+});
+
+
 const handle = app.getRequestHandler()
 
 app.prepare().then(() => {
 
     const server = express();
+    server.use(express.json())
+    server.get('/api/v1/movies', (req, res) => {
+        return res.json({
+            message: 'hello World'
+        })
+    });
+    server.post('/api/v1/movies', (req, res) => {
+        const movie = req.body;
+        console.log(JSON.stringify(movie))
+        return res.json({
+            ...movie,
+            author: 'John Smith',
+            sex: 'great'
+        })
+    });
+    server.patch('/api/v1/movies/:id', (req, res) => {
+        const {
+            id
+        } = req.params;
+        return res.json({
+            message: `Updating post of id :${id}`
+        })
+    });
+    server.delete('/api/v1/movies/:id', (req, res) => {
+        const {
+            id
+        } = req.params;
+        return res.json({
+            message: 'Delete post of id: {id}'
+        })
+    });
     server.get('*', (req, res) => {
         return handle(req, res)
     })

9d697874739fe36af979e3213943f0231b8cb2e5
Yohan <yohan@yohanpro.com>
2020-01-05 20:28:39 +0900

    router with router로 대체

--- pages/movies/[id]/edit.js
+++ pages/movies/[id]/edit.js
@@ -1,24 +1,35 @@
 import React, { Component } from 'react';
+import Router, { withRouter } from 'next/router';
 import MovieCreateform from '../../../components/movieCreateForm';
-import { getMovieById } from '../../../actions';
+import { getMovieById, createMovie } from '../../../actions';
 
 
 class EditMovie extends Component {
 
-    static async getInitialProps({ query }) {
+    static async getInitialProps({ query, router }) {
         const movie = await getMovieById(query.id);
-        return { movie };
+        const myRouter = router;
+        return { movie, myRouter };
     }
+    handleCreateMovie(movie) {
 
+        createMovie(movie).then((movies) => {
+
+            Router.push({
+                pathname: '/',
+                // query: { name: 'Someone' }
+            });
+        });
+    };
     render() {
         const { movie } = this.props;
         return (
             <div className="container">
                 <h1>Edit the movie</h1>
-                <MovieCreateform initialData={movie} />
+                <MovieCreateform handleFormSubmit={this.handleCreateMovie} initialData={movie} />
             </div>
         );
     }
 }
 
-export default EditMovie;
+export default withRouter(EditMovie);
